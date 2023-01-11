6ee2982c149bf5866d5679e3b89d097b04d6bcc7
Jordan McDaniel <jdon8600@gmail.com>
2023-01-11 11:16:15 -0500

    CSS atempts

--- components/ui/Nav.js
+++ components/ui/Nav.js
@@ -1,23 +1,36 @@
-import classes from './nav.module.css'
-import { signOut } from 'next-auth/react';
-import Link from 'next/link';
+import classes from "./nav.module.css";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
 
 function Nav() {
   return (
-    <div >
-    <ul className={classes.nav}>
+    <div>
+      <ul className={classes.nav}>
+        <li className={classes.navItems}>Inspection Updating Tool</li>
         <li className={classes.navItems}>
-            Inspection Updating Tool
+          <Link href="/" className={classes.navItems.a}>
+            Home
+          </Link>
         </li>
-      <li className={classes.navItems}>
-        <Link  href="/" className={classes.navItems.a}>Home</Link>
-      </li>
-      <li className={classes.navItems}>
-        <Link  href="/projects" className={classes.navItems.a}>Project</Link>
-      </li>
-      <button className={classes.btn} onClick={()=>signOut()}>Logout</button>
-    </ul>
-</div>
+        <li className={classes.navItems}>
+          <Link
+            href="/projects"
+            style={{
+              display: block,
+              color: white,
+              textAlign: center,
+              textDecoration: none,
+              paddingBottom: 16,
+            }}
+          >
+            Project
+          </Link>
+        </li>
+        <button className={classes.btn} onClick={() => signOut()}>
+          Logout
+        </button>
+      </ul>
+    </div>
   );
 }
 
